aa029abe56d9d87d666f74ea39d0bfc91cd8fe14
Giovani Girardi <giovanigg08@gmail.com>
2024-08-17 15:22:47 -0300

    feat: :sparkles: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
+export function App() {
+  return (
+    <h1 className="flex justify-center items-center h-screen bg-muted">
+      <Button variant="default">Hello World :)</Button>
+    </h1>
+  );
+}
