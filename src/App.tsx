1895f2861b04c69ee1e4f3a94ff26bb28c19bc4c
Giovani Girardi <giovanigg08@gmail.com>
2024-08-17 18:41:10 -0300

    chore: :mag: configured react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./global.css";
 
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes/router";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Marketplace" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
